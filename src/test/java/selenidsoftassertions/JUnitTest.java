ce30d14cf29b750401fbc088ad59e5a96661a5c3
DSaltykov <spitfiredia@gmail.com>
2023-08-24 10:30:13 +0300

    added change registr

--- src/test/java/selenidsoftassertions/JUnitTest.java
+++ src/test/java/selenidsoftassertions/JUnitTest.java
@@ -1,12 +1,10 @@
-package SelenidSoftAssertions;
+package selenidsoftassertions;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
