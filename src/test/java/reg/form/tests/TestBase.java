3911fe66846aacce18e13318147440e9ffe1aacd
DSaltykov <spitfiredia@gmail.com>
2023-09-11 14:20:30 +0300

    add configuration

--- src/test/java/reg/form/tests/TestBase.java
+++ src/test/java/reg/form/tests/TestBase.java
@@ -3,6 +3,9 @@ package reg.form.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 @Tag("remote")
 public class TestBase {
@@ -15,7 +18,15 @@ public class TestBase {
         Configuration.browserSize = "1920х1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
-        Configuration.remote= "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote= ("https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC" , true,
+                "enableVideo" , true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
 
     }
 
